e33e249b6d581359bbca552f9d69580108f40024
Fazil Basheer <fazil.basheer@quester.xyz>
2020-05-08 03:58:03 +0530

    add hidable revealables

--- app/javascript/controllers/revealable_controller.js
+++ app/javascript/controllers/revealable_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["colorable"]
+  static targets = ["colorable", "hidable"]
 
-  REVEAL_DURATION = 3000 // seconds
+  REVEAL_DURATION = 3333 // milliseconds
   REVEAL_COLOR = ''
+  REVEAL_VISIBILITY = 'visible'
   
   HIDE_COLOR = 'transparent'
+  HIDE_VISIBILITY = 'hidden'
 
   connect() {
     this.waitAndHideRevealables()
@@ -21,13 +23,26 @@ export default class extends Controller {
     setTimeout(() => this.hideRevealables(), this.REVEAL_DURATION)
   }
 
-  revealReveables() { this.setColorablesColor(this.REVEAL_COLOR) }
+  revealReveables() {
+    this.setColorablesColor(this.REVEAL_COLOR)
+    this.setHidablesVisibility(this.REVEAL_VISIBILITY)
+  }
 
-  hideRevealables() { this.setColorablesColor(this.HIDE_COLOR) }
+  hideRevealables() {
+    this.setColorablesColor(this.HIDE_COLOR)
+    this.setHidablesVisibility(this.HIDE_VISIBILITY)
+  }
 
   setColorablesColor(color) {
     this.colorableTargets.forEach(element => this.setElementColor(element, color))
   }
 
-  setElementColor(element, color) { element.style.color = color }
+  setHidablesVisibility(visibility) {
+    this.hidableTargets.forEach(element => this.setElementVisibility(element, visibility))
+  }
+
+  setElementColor(element, color) { this.setElementAttributeValue(element, 'color', color) }
+  setElementVisibility(element, visibility) { this.setElementAttributeValue(element, 'visibility', visibility) }
+
+  setElementAttributeValue(element, attribute, value) { element.style[attribute] = value }
 }
