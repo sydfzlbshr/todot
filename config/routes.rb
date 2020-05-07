d0a1f5b15ad7c4aa4d930f47c8ca5797509c184a
Fazil Basheer <fazil.basheer@quester.xyz>
2020-05-07 08:41:32 +0530

    change root to todos index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :announcements
     resources :notifications
 
-    root to: "users#index"
+    root to: "todos#index"
   end
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root to: 'todos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/service-worker.js' => "service_worker#service_worker"
