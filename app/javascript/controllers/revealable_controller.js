68eb72cdca51635878c3783474f67c3ba314781d
Fazil Basheer <fazil.basheer@quester.xyz>
2020-05-08 01:42:53 +0530

    setup revealables

---
+++ app/javascript/controllers/revealable_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["revealable"]
+
+  REVEAL_DURATION = 3000 // seconds
+  REVEAL_COLOR = ''
+  
+  HIDE_COLOR = 'transparent'
+
+  connect() {}
+
+  reveal() {
+    this.revealReveables()
+    this.waitAndHideRevealables()
+  }
+
+  waitAndHideRevealables() {
+    setTimeout(() => this.hideRevealables(), this.REVEAL_DURATION)
+  }
+
+  revealReveables() { this.setRevealablesColor(this.REVEAL_COLOR) }
+
+  hideRevealables() { this.setRevealablesColor(this.HIDE_COLOR) }
+
+  setRevealablesColor(color) {
+    this.revealableTargets.forEach(element => this.setElementColor(element, color))
+  }
+
+  setElementColor(element, color) { element.style.color = color }
+}
