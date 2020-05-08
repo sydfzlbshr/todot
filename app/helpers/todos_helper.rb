170988e733823cd52016cf7b361fff5e7ced86e6
Fazil Basheer <fazil.basheer@quester.xyz>
2020-05-08 23:57:23 +0530

    extract helper method due_in_less_than_class

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 module TodosHelper
+  def due_in_less_than_class(todo)
+    base = 'due-in-less-than'
+    if todo.due_at < 1.hour.from_now
+      "#{base}-an-hour"
+    elsif todo.due_at < 2.hours.from_now
+      "#{base}-two-hours"
+    elsif todo.due_at < 4.hours.from_now
+      "#{base}-four-hours"
+    else
+      ''
+    end
+  end
 end
