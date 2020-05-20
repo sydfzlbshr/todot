1928aff2e8a64ec0bcba9500577e9cb819c91c9d
Fazil Basheer <fazil.basheer@quester.xyz>
2020-05-20 11:35:37 +0530

    add motivational gifs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def image_urls
+    [
+      "https://i.pinimg.com/originals/6d/10/ff/6d10ff9e8c5b1dcffeba60c2d99f3fd0.gif"
+    ]
+  end
+
   def bootstrap_class_for(flash_type)
     {
       success: "alert-success",
